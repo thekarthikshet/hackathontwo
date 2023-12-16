f9a2b7e29778a7d5e02046f6457e582cf1d55746
thekarthikshet <shetkarthik8888@gmail.com>
2023-12-16 10:44:01 +0530

    init

---
+++ src/main/java/com/bbi/ps7/Ps7Application.java
@@ -0,0 +1,13 @@
+package com.bbi.ps7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbi/ps7/Ps7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbi.ps7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
